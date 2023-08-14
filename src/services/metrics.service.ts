30315d92b13ff0bb2c3dcd7484f870176f12ab85
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-08-14 16:21:47 +0200

    [UPDATE]

--- src/services/metrics.service.ts
+++ src/services/metrics.service.ts
@@ -1,8 +1,8 @@
-import { Firestore, doc, setDoc } from "@firebase/firestore";
-import { collection, addDoc } from "firebase/firestore"; 
+import {  Firestore, doc, setDoc, collection, addDoc, updateDoc, DocumentData, DocumentReference,getDoc  } from "firebase/firestore";
+
 import { v4 as uuidv4 } from 'uuid';
 import { Request, Response, NextFunction} from "express";
-import { logger } from "../configuration/logger";
+import { logger as LOG } from "../configuration/logger";
 import { FirebaseApp } from "firebase/app";
 
 export interface IMetricsServicePayload {
@@ -12,101 +12,105 @@ export interface IMetricsServicePayload {
 }
 
 
-const checkUserHasFootprint = async (cookies: Record<string, any>, req:Request, res:Response, firestore:Firestore): Promise<string|null> => {
+export interface ElementPayload {
+    id: string,
+    htmlTag: string,
+    action: {
+        name: string,
+        count: number
+    }
+}
 
-    if ( req.body?.fromUrl && req.body?.applicationId) {
-        if ( cookies && Object.keys(cookies).length !== 0 ) { 
-            if (cookies[req.body.applicationId]) {
-                if ( !cookies[req.body.applicationId].split(',').includes(req.body.fromUrl) ) {
-                    console.log("2")
+/**
+ * Handle cookie and create / update entry in database if it's required
+ */
+const upsertMetrics = async ( cookies: Record<string, any>, req:Request, res:Response, firestore:Firestore): Promise<string|null> => {
 
-                    // New path detected, update existing cookie
-                    let newValue = `${cookies[req.body.applicationId]},${req.body.fromUrl}`;
-                    res.cookie( req.body.applicationId, newValue, { maxAge: 2592000000, httpOnly: false });
+    if ( !req.body?.fromUrl || !req.body?.applicationId || !req.body?.elements) {
+        LOG.error('fromUrl or applicationId or elements is missing in payload');
+        return null;
+    }
 
-                    await setDoc(doc(firestore, "users",`${newValue.split(",")[0]}` ), {
-                        "test": 2
-                    });
+    try {
 
-                    return newValue.split(",")[0];
-                } else {
-                    // Not a new path to add
-                    console.log("3")
-                    
-                    return cookies[req.body.applicationId].split(',')[0];
-                }
+        if ( cookies && Object.keys(cookies).length !== 0 && Object.keys(cookies).includes(req.body.applicationId)) {
+
+            // Check if entry in database exists for this uuid
+            const id = cookies[req.body.applicationId].split(",")[0];
+
+            const docSnapshot = await getDoc(doc(firestore, "users", `${id}`));
+            if (docSnapshot.exists()) {
+                // update
+                LOG.info("Update entry : " + id);
+                const { pathsVisited } = docSnapshot.data();
+                
+                await setDoc(doc(firestore, "users",`${id}` ), {
+                    "updatedAt": new Date().toISOString(),
+                    "pathsVisited": {
+                        ...pathsVisited,
+                        [`${req.body.fromUrl}`]: [...req.body.elements as Array<ElementPayload>]
+                    }
+                });
+
+                return id;
+
             } else {
-                // Cookie not found
-                return null;
+
+                // create new entry ( imagine user delete cookie during process)
+                LOG.info("create new entry " + id);
+                const val = `${uuidv4()},${req.body.fromUrl}`;
+                res.cookie( req.body.applicationId, val, { maxAge: 2592000000, httpOnly: false });
+                await setDoc(doc(firestore, "users",`${val.split(",")[0]}` ), {
+                    "createdAt": new Date().toISOString(),
+                    "updatedAt": null,
+                    "pathsVisited": {
+                        [`${req.body.fromUrl}`]: [...req.body.elements as Array<ElementPayload>]
+                    }
+                });
+    
+                return val.split(",")[0];
+                
             }
+
         } else {
 
-            // Create new cookie
-            console.log("1")
-
+            LOG.info('Cookie not found for ' + req.body.applicationId + " create new one ...");
+            // create new cookie
             const val = `${uuidv4()},${req.body.fromUrl}`;
             res.cookie( req.body.applicationId, val, { maxAge: 2592000000, httpOnly: false });
             await setDoc(doc(firestore, "users",`${val.split(",")[0]}` ), {
-                "test": 1
+                "createdAt": new Date().toISOString(),
+                "updatedAt": null,
+                "pathsVisited": {
+                    [`${req.body.fromUrl}`]: [...req.body.elements as Array<ElementPayload>]
+                }
             });
 
             return val.split(",")[0];
         }
+        
 
-    } else {
-        logger.error('fromUrl or applicationId is missing in payload')
+    } catch ( e ) {
+        LOG.error(e);
+        res.clearCookie(req.body.applicationId);
         return null;
     }
+
+    return null;
 }
+
 
 export const metricsService = {
 
     saveMetrics: async (firestore:Firestore, payload: IMetricsServicePayload) => {
-        const id: string | null = await checkUserHasFootprint(payload.req.cookies, payload.req, payload.res, firestore);
-        console.log(id);
+        const id: string | null = await upsertMetrics(payload.req.cookies, payload.req, payload.res, firestore);
+        if ( id === null ) {
+            LOG.error('Failed to create / analysis cookie for footprint');
+            return;
+        } else {
+            LOG.info(`user id: ${id}`)
+
+        }
     }
 
 };
-
-    // /**
-    //  * 
-    //  * @param firestore 
-    //  * @param ip 
-    //  * @param cookies [Object: null prototype] {} (empty object without inherited properties of Object.prototype)
-    //  */
-    // saveMetrics: async (firestore:Firestore, ip:string | null, cookies: Record<string, any>) => {
-
-    //     checkUserHasFootprint(ip, cookies);
-
-    //     try {
-
-    //         let docRef;
-
-    //         if ( ip === null ) {
-    //             docRef = doc(firestore, "users", `${ip}`);
-    //         } else {
-    //             // auto generate ID
-    //             docRef = doc(firestore, "users",`anon_${uuidv4()}` );
-    //         }
-
-    //         const resp = await setDoc(docRef, {
-    //             "/": {
-    //                 "btn-id-1" : {
-    //                     "impressions": 1,
-    //                 }
-    //             },
-    //             "/dashboard": {
-    //                 "btn-id-2" : {
-    //                     "impressions": 10,
-    //                 }
-    //             },
-    //         });
-
-    //         console.log(resp);
-
-    //     } catch ( e ) {
-            
-    //         console.log(e);
-
-    //     }
-    // }
