f42a589a9ca0427b97e00a08d6274cd144499597
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-09-01 00:14:29 +0200

    e

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -5,33 +5,39 @@ const stripe = loadStripe();
 export const webhookController = Router();
 
 webhookController.post('/', async ( req:Request, res:Response, _next:NextFunction ) => {
-    
+
     const sig = req.headers['stripe-signature'];
     let event;
 
     try {
         event = stripe.webhooks.constructEvent(req.body, sig!, process.env.WHSEC!);
+        
+        // const resp = await stripe.paymentLinks.listLineItems( 'plink_1Ni1vxG9WAND04yWTDpYg6ws', {});
+        // console.log(resp);
 
         switch ( event.type ) {
             case 'charge.succeeded':
                 const chargeData:any = event.data.object;
-            
                 const pi:any = (await stripe.charges.retrieve(chargeData.id)).payment_intent;
                 console.log("now<ay")
                 const paymentIntent = await stripe.paymentIntents.retrieve(pi);
-                console.log(paymentIntent);
+                console.log(paymentIntent.metadata);
                 console.log("DAMN")
                 break;
             case 'checkout.session.completed':
                 console.log("session completed");
                 const sessionData:any = event.data.object;
 
-                if ( sessionData.payment_status === 'paid' ) {
-                    
-                    console.log(sessionData)
-                    console.log("=========================================")
+                if (sessionData.payment_status === 'paid') {
+                        // Récupérer les line_items associés à cette session
+                        const lineItems = await stripe.checkout.sessions.listLineItems(sessionData.id);
+
+                        lineItems.data.forEach(item => {
+                            console.log(`Produit: ${item.description}, Quantité: ${item.quantity}`);
+                        });
+
+                        console.log("=========================================")
                 }
-                
 
             default:
                 // No action planned for this event
