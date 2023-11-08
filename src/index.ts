aa903a7c871f1f80e30258dab3c38fe6d937fd81
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-09 00:27:06 +0100

    [UPDATE]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config();
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import { logger } from './configuration/logger';
@@ -9,6 +9,7 @@ import { webhookController } from './controllers/webhook.controller';
 import { paymentLinkController } from './controllers/paymentLink.controller';
 import { orderController } from './controllers/orders.controller';
 import path from 'path';
+import jwt from 'jsonwebtoken';
 
 const PORT:number = 9000;
 const app = express();
@@ -28,7 +29,31 @@ app.get('/health' , ( req, res ) => {
    res.status(200).send("HEALTHY"); 
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+
+const middlewareDetectBaconPixelOpen = (req:Request, res:Response, next:NextFunction) => {
+    if ( req.query.email ) {
+        const tokenEmail:string = req.query.email as string;
+
+        try {
+            console.log("decoding token for email ...")
+            const email = jwt.verify(tokenEmail, process.env.BACON_PIXEl_TOKEN_SECRET as string);
+            console.log("email decoded (email open): ", email);
+        } catch ( e ) {
+            console.log("Fail to open token for email : ", e);
+        }
+    } else {
+        console.log("Email token not provided while calling /static")
+    }
+
+    next();
+};
+
+app.use('/static', middlewareDetectBaconPixelOpen,express.static(path.join(__dirname, 'public')))
+
+app.use((req,res,next) => {
+    logger.info(`Request ${req.method} ${req.url}`);
+    next();
+})
 
 app.use('/api', express.json(), productController )
 app.use('/api', express.json(), paymentLinkController )
